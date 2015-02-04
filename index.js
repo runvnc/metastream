f3228c8932f0841da93f2dab22d034dba84ce846
Jason Livesay <ithkuil@gmail.com>
2015-02-04 20:29:27 +0000

    seems to be working

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ module.exports = function(stream) {
     return obj[Object.keys(obj)[0]];
   }
 
-  var parts =
+  function outBuff(buff) {
+    str = '';
+    for (var i=0; i< buff.length; i++ ){
+      str += '['+buff.readUInt8(i)+'] ';
+    }
+    return str;
+  }
+
+  var _parts =
     [ { 'totalLength': 4 },
       { 'jsonLength': 2 },
       { 'json': Infinity },
@@ -19,6 +27,7 @@ module.exports = function(stream) {
 
   var _partName = null,
       _pos = 0,
+      _partPos = 0,
       _fullBuffer = new Buffer(''),
       _partNum = Infinity,
       _remaining = Infinity,
@@ -29,41 +38,59 @@ module.exports = function(stream) {
       _partNum = 0;
     } else {
       _partNum += 1;
-      _part = parts[_partNum];
-      _remaining = firstProp(_part);
-      _partName = Object.keys(_part)[0];
-      _buffer = new Buffer(_remaining);
-      _pos = 0;
-    } 
+    }
+    _partPos = 0;
+    _part = _parts[_partNum];
+    console.log('nextPart _part = ' + JSON.stringify(_part));
+    _remaining = firstProp(_part);
+    _partName = Object.keys(_part)[0];
+    console.log('^^^^^^ ' + _remaining);
+    _buffer = new Buffer(_remaining);
   }
 
   self.read = function(buffer) {
+    console.log('top of read');
     if (_partNum === Infinity) nextPart();
+    console.log("_buffer.length is " + _buffer.length);
     var bytesCopied = Math.min(buffer.length, _remaining);
 
-    buffer.copy(_buffer, _pos, bytesCopied);
+    console.log('copying ' + bytesCopied + ' bytes');    
+    buffer.copy(_buffer, _partPos, _pos, _pos+bytesCopied);
     _pos += bytesCopied;
+    _partPos += bytesCopied;
     _remaining -= bytesCopied;
-    
+    console.log('_buffer is now ' + outBuff(_buffer));
     if (_remaining > 0) { 
-      return;
+      console.log('more remaining in part');
+      nextPart();
+      return self.read(buffer);
     } else {
-      _fullBuffer = Buffer.concat(_fullBuffer, _buffer);
+      console.log('done with part');
+      _fullBuffer = Buffer.concat([_fullBuffer, _buffer]);
       if (_partNum === _parts.length - 1) {
+        console.log('done with all');
+        console.log('**'+_buffer.toString()+'**');
         var extracted = self.extract(_fullBuffer);
+        _partNum = Infinity;
+        _pos = 0;
+        _partPos = 0;
         self.onData(extracted.meta, extracted.buffer);
       } else {
-        switch _partName {
+        console.log('handling part info');
+        switch (_partName) {
           case 'totalLength':
-            _parts.data = _buffer.readUInt32LE();
+            _parts[3].data = _buffer.readUInt32LE(0);
+            console.log('read data length ' + _parts.data);
             break;
           case 'jsonLength':
-            _parts.json = _buffer.readUInt16LE() - 6;
-            _parts.data -= _parts.json;
+            _parts[2].json = _buffer.readUInt16LE(0);
+            console.log('read json length ' + _parts[2].json);
+            _parts[3].data -= _parts[2].json + 6;
             break;
         }
-
+        console.log('going to next');
         nextPart();
+        return self.read(buffer);
       }
     }
   }
@@ -77,6 +104,7 @@ module.exports = function(stream) {
   }
 
   self.stream.on('data', function(data) {
+    console.log('data event on stream');
     self.read(data);
   });
 
@@ -85,16 +113,18 @@ module.exports = function(stream) {
     var jsonBuffer = new Buffer(json);
     var totalLength = jsonBuffer.length + 4 + 2 + buffer.length;
     var outBuffer = new Buffer(totalLength);
-    outBuffer.writeUint32LE(totalLength);
-    outBuffer.writeUInt16LE(jsonBuffer.length, 0);
-    outBuffer.write(json, 4);
+    outBuffer.writeUInt32LE(totalLength,0);
+    outBuffer.writeUInt16LE(jsonBuffer.length, 4);
+    outBuffer.write(json, 6);
     buffer.copy(outBuffer, 4 + 2 + jsonBuffer.length);
     return outBuffer;
   }
 
   this.write = function(meta, buffer) {
     var newBuffer = self.addMeta(buffer, meta);
-    stream.write(newBuffer);
+    self.stream.write(newBuffer);
+    console.log('wrote ' + newBuffer.length);
+    console.log('buffer wrote was ' + outBuff(newBuffer));
   }
 
   return self;
--- test.js
+++ test.js
@@ -1,21 +1,26 @@
 net = require('net');
-metastream = require('./metastream');
+Metastream = require('./index');
 
 var server = net.createServer(function(c) {
-  var outp = metastream(c);
+  console.log('server recvd conn');
+  var outp = new Metastream(c);
   outp.write({id: 'bob'}, new Buffer('Bob Test'));
+  console.log('server wrote data');
   outp.on(function(meta, data) {
     console.log('server recvd from ' + meta.id + ': ' + data.toString());
   });
+  setTimeout(function() { process.exit() }, 250);
 });
 
 server.listen(6345);
+console.log('Server listening');
 
 var client = net.connect({port: 6345}, function() {
-  var conn = metastream(client);
+  console.log('client connected to server');
+  var conn = new Metastream(client);
   conn.on(function(meta, data) {
-    console.log('client recvd from ' + meta.id + ': ' + data.toString();
-    conn.write({id: 'tom'}, "Tom received data " + data.toString() + ' back at yah');
+    console.log('client recvd from ' + meta.id + ': ' + data.toString());
+    conn.write({id: 'tom'}, new Buffer("Tom received data " + data.toString() + ' back at yah'));
   });
 });
 
